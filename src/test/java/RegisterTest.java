2e2d4fdd3284c0df765bb37d888d22f712255f32
alexcrtCS <alexandrucarata@coherentsolutions.com>
2023-01-27 16:17:08 +0200

    - Added test classes

---
+++ src/test/java/RegisterTest.java
@@ -0,0 +1,21 @@
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pageclasses.AccountPage;
+import pageclasses.RegisterPage;
+import util.BaseTest;
+
+@Epic("Luma Store - Account")
+@Story("Account Registration")
+public class RegisterTest extends BaseTest {
+
+    @Test
+    @Feature("Ability to Register New User")
+    @AllureId("AP-1")
+    @Description("Check if new user is successfully registered")
+    public void registerUserTest() {
+        RegisterPage registerPage = landingPage.clickCreateAnAccountButton();
+        AccountPage accountPage = registerPage.registerAccount();
+        Assertions.assertTrue(accountPage.isRegistrationMsgDisplayed());
+    }
+}
